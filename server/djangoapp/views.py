3b9096e8dd1498241670fb8cde71cd6c0afd8803
ammarya5i <ammaremran03@gmail.com>
2026-06-23 09:33:55 -0400

    Add Dockerfile, entrypoint, deployment.yaml and sentiment fallback

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -95,7 +95,8 @@ def get_dealer_reviews(request, dealer_id):
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            review_detail['sentiment'] = (
+                response['sentiment'] if response else "neutral")
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
@@ -128,3 +129,4 @@ def get_cars(request):
             "CarMake": car_model.car_make.name
         })
     return JsonResponse({"CarModels": cars})
+    
